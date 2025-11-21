6841f70836c96625599e3da8bac7bedd30d1e637
Ravi Guru <rockingrocky4@yahoo.com>
2025-11-21 16:50:22 +0530

    Dummy commit to trigger Jenkins

--- random/wi.py
+++ random/wi.py
@@ -1,15 +1,39 @@
+# s = "i loVE Python"
+# # i LOve pYTHON
+# words = s.split()
+# #print(words)
+# li = []
+# for word in words:
+#     converted_list = []
+#     for j in range(len(word)):
+#             letter = word[j]
+#         if j == 0:
+#             converted_list.append(letter)
+#             continue
+#         temp = ord(letter)
+#         if 65 <= temp <= 90:
+#             converted_list.append(chr(temp + 32))
+#         elif 97 <= temp <= 122:
+#             converted_list.append(chr(temp - 32))
+#         else:
+#             converted_list.append(letter)
+#     li.append("".join(converted_list))
+# print(" ".join(li))
+#
+#
 s = "i loVE Python"
-# i LOve pYTHON
 words = s.split()
-#print(words)
 li = []
+
 for word in words:
     converted_list = []
     for j in range(len(word)):
         letter = word[j]
+
         if j == 0:
             converted_list.append(letter)
             continue
+
         temp = ord(letter)
         if 65 <= temp <= 90:
             converted_list.append(chr(temp + 32))
@@ -17,7 +41,7 @@ for word in words:
             converted_list.append(chr(temp - 32))
         else:
             converted_list.append(letter)
-    li.append("".join(converted_list))
+
+    li.append("".join(converted_list))   # ← FIX HERE
+
 print(" ".join(li))
-
-
